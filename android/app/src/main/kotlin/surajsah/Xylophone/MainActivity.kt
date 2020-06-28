5135344cf9e31ee83ca1f2a1ad7b1247e18ca219
surrajj20 <shah.suraj1200@gmail.com>
2020-06-28 13:55:27 +0530

    app setup

---
+++ android/app/src/main/kotlin/surajsah/Xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package surajsah.Xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
